5ddf9b6182c8e1af8bd397e4e3d25d08033763c3
Shubham <shubhambadgujar222@gmail.com>
2022-02-22 23:06:03 +0530

    mukula (data added)

---
+++ Mukula/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+    public static void main(String[] args)
+    {
+
+     System.out.print("Hello World");
+     int num1=6;
+     int num2=2;
+     int num3=5;
+     int sum=num1+num2+num3;
+     System.out.println(sum);
+
+
+    }
+    
+}
+
+  
